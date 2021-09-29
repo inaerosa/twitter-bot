a5f85067d3070b630b1fb8e0ee60c6e06e198441
Inaê Rosa <inaedb.rosa@gmail.com>
2021-09-29 18:51:25 -0300

    random number in tweetIt

--- bot.js
+++ bot.js
@@ -20,22 +20,25 @@ function gotData(err,data,response){
 }
 
 //post
-tweetIt();
-
 function tweetIt() {
 
-    var tweet = {
-      status: `'#tweeting from node.js'`
+  var r = Math.floor(Math.random()*100);
+
+  var tweet = {
+    status: `here is a random number: ${r} in node.js`
+  }
+
+  T.post('statuses/update', tweet, tweeted)
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("It worked")
     }
-  
-    T.post('statuses/update', tweet, tweeted)
-  
-    function tweeted(err, data, response) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("It worked")
-      }
-    }
-}  
-  
+  }
+
+}
+
+tweetIt()
+
