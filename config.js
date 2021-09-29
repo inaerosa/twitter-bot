064439e4ba9504be25b1bde953fc738e3c6b9c2b
Inaê Rosa <inaedb.rosa@gmail.com>
2021-09-29 18:14:11 -0300

    add config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+//in this file you need add your keys and tokens
+
+module.exports ={
+    consumer_key:         '....',
+    consumer_secret:      '....',
+    access_token:         '....',
+    access_token_secret:  '....',
+}
