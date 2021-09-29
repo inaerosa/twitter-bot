1c4c09cfb58afded7d166a30453dc75b87bead6f
Inaê Rosa <inaedb.rosa@gmail.com>
2021-09-29 18:18:48 -0300

    post tweet

--- bot.js
+++ bot.js
@@ -23,4 +23,15 @@ function gotData(err,data,response){
 var tweet = {
     status: '#tweeting from node.js'
   }
+
+T.post('statuses/update', tweet, tweeted)
+
+function tweeted(err,data,response){
+  if(err){
+    console.log(err);
+  } else{
+    console.log("It worked")
+  }
+}
+
   
